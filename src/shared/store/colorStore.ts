05f7cb0a9072dda53dd630528b98be69332debe6
israel-gs <israelgs83@gmail.com>
2025-02-02 19:24:27 -0500

    feat: add color name management to color store

--- src/shared/store/colorStore.ts
+++ src/shared/store/colorStore.ts
@@ -1,22 +1,32 @@
 import { createStore } from 'zustand/vanilla'
-import { generateTailwindPalette, getRandomHexColor } from '../utils/colorUtils'
+import {
+  generateTailwindPalette,
+  getColorName,
+  getRandomHexColor
+} from '../utils/colorUtils'
 import { useStore } from 'zustand'
 
 interface ColorStore {
   currentColor: string
   palette: Record<number, string> | null
+  colorName: string | null
   setCurrentColor: (color: string) => void
 }
 
 const initialRandomColor = getRandomHexColor()
 const initialPalette = generateTailwindPalette(initialRandomColor)
+const initialColorName = getColorName(initialRandomColor)
 
 const colorStore = createStore<ColorStore>(set => ({
   currentColor: initialRandomColor,
   palette: initialPalette,
+  colorName: initialColorName,
   setCurrentColor: (color: string) => {
-    console.log('setting color', color)
-    return set({ currentColor: color, palette: generateTailwindPalette(color) })
+    return set({
+      currentColor: color,
+      palette: generateTailwindPalette(color),
+      colorName: getColorName(color)
+    })
   }
 }))
 
